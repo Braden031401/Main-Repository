e93b3fea7ee1fb5dedf892c143cf6dc6ef0476e0
Sky <kyutiexd@gmail.com>
2017-10-17 08:42:52 -0500

    update

--- Final Project/src/FinalApplication/QuestionTwo.java
+++ Final Project/src/FinalApplication/QuestionTwo.java
@@ -53,7 +53,7 @@ public class QuestionTwo extends JFrame{
 		button1.setMnemonic(KeyEvent.VK_B);
 		button1.setActionCommand("1");
 		button1.addActionListener(b -> JOptionPane.showMessageDialog(null, "Correct!"));
-		button1.addActionListener(a -> QuestionThree.main(null));
+		button1.addActionListener(b -> QuestionThree.main(null));
 		
 		setSize(SIZE2, SIZE2);
 		con2.setLayout(new FlowLayout());
---
+++ Final Project/src/FinalApplication/threeLoop.java
@@ -0,0 +1,11 @@
+package FinalApplication;
+
+public class threeLoop 
+{
+
+	public static void main(String[] args) 
+	{
+		for(int i = 1; i < 23; i++)
+			System.out.println("count is:" + i);
+	}
+}
