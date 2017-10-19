e5c535ca092b71464c3276bf0eb6aeb60eabb9ee
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-10-19 09:04:14 -0500

    Final Update...

--- Final Project/src/FinalApplication/BaseQuestion.java
+++ Final Project/src/FinalApplication/BaseQuestion.java
@@ -66,16 +66,7 @@ public class BaseQuestion extends JFrame{
 		button1.setForeground(Color.WHITE);
 		button1.setMnemonic(KeyEvent.VK_B);
 		button1.setActionCommand("1");
-		button1.addActionListener(b -> {
-			try {
-				Score.main(null);
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		});
-		button1.addActionListener(b -> dispose());
-		button1.addActionListener(b -> frame.dispose());
+		button1.addActionListener(b -> JOptionPane.showMessageDialog(null, "Wrong Answer"));
 		
 		setSize(SIZE2, SIZE2);
 		con2.setLayout(new FlowLayout());
